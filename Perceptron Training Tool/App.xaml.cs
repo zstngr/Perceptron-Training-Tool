551f92111699697807a5d3b41e18d236ea00849e
Артур Закиров <zstngr@gmail.com>
2019-05-27 14:49:54 +0300

    Добавьте файлы проекта.

---
+++ Perceptron Training Tool/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Perceptron_Training_Tool
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
